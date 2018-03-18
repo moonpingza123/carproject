cb7c7006a67c29d0274e565ac4d47a4e38b35f69
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2018-03-18 11:23:57 +0700

    democarjaidee

---
+++ DemoCar.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html>
+<head>
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+	  <title>Fixed Menu Example - Semantic</title>
+	  <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS/semantic.css">
+
+	  
+	  
+	  <style type="text/css">
+	  <head>
+  	  </style>
+ 
+	  <style>
+	  		body {
+	    	padding: 1em;
+	  	}
+	  		.spaced > .button {
+	    	margin-bottom: 1em;
+	 	}
+	  	body {
+		    background-color: #FFFFFF;
+		  }
+		  .ui.menu .item img.logo {
+		    margin-right: 1.5em;
+		  }
+		  .main.container {
+		    margin-top: 7em;
+		  }
+		  .wireframe {
+		    margin-top: 2em;
+		  }
+		  .ui.footer.segment {
+		    margin: 5em 0em 0em;
+		    padding: 5em 0em;
+		  }
+		  .ui.green1.buttons .button,
+		  .ui.green1.button {
+ 			background-color: #53c68c;
+  			color: #FFFFFF;
+  			text-shadow: none;
+		  }
+		  .ui.orange1.buttons .button,
+		  .ui.orange1.button {
+ 			background-color: #ff471a;
+  			color: #FFFFFF;
+  			text-shadow: none;
+		  }
+		  
+		  
+	  </style>
+
+	  <script type="text/javascript" src="Semantic-UI-CSS/components/semantic.js"></script>
+	  <script>
+	  $(document)
+	    .ready(function() {
+
+	      // fix menu when passed
+	      $('.masthead')
+	        .visibility({
+	          once: false,
+	          onBottomPassed: function() {
+	            $('.fixed.menu').transition('fade in');
+	          },
+	          onBottomPassedReverse: function() {
+	            $('.fixed.menu').transition('fade out');
+	          }
+	        })
+	      ;
+
+	      // create sidebar and attach to menu open
+	      $('.ui.sidebar')
+	        .sidebar('attach events', '.toc.item')
+	      ;
+
+	    })
+	  ;
+	  </script>
+</head>
+	  
+<body>
+	
+	<div class="ui inverted segment" style="background-color:#53c68c;">
+  		<div class="ui inverted secondary menu">
+    		<div class="ui container">
+	    		<a class="active item">Home</a>
+	    			<div class="right menu">
+			    		<a class="item">อู่รถยนต์</a>
+			    		<a class="item">ช่างซ่อม</a>
+				   		<a class="item">นัดหมาย</a>
+				    	<a class="item">คลังอะไหล่</a>
+				    	<a class="item">ข้อมูลส่วนตัว</a>
+			      	<div class="item">
+			      		<i class=" icons">
+  							<i class="black big thin circle icon"></i>
+  							<i class="user icon"></i>
+						</i>
+			        	<a class="ui red button">สมัครใช้งาน</a>
+			      	</div>
+			      	<div class="item">
+			        	<a class="ui primary button">ลงชื่อเข้าใช้</a>
+			      	</div>
+	    		</div>
+	  		</div>
+  		</div>
+	</div>
+	
+	<div class="ui container">
+  		<div class="ui two column stackable grid container">
+  			<div class="eleven wide column">
+          		<h2 class="blue ui header"><i class="black car icon"></i>CarJaidee.com     Hotline 1100</h2>
+          		<p>Pay for just the miles you  drive. Find and access vehicles via your smartphone</p>
+        	</div>
+        	<div class="four wide column ">
+          		<button class="inverted blue ui huge right floated button ">คำนวณค่าใช้จ่าย</button>
+        	</div>
+		</div>
+	</div><br>
+
+	<div class="ui container">
+          <img class="ui fluid image"  src="img/map.jpg">
+    </div><br>
+    <div class="ui container">      	
+	    <div class="ui huge action input" >
+			<input type="text huge" placeholder="Search for a car in town, postcode etc." style="width: 550px;">
+			<button class="ui button big" style="background-color:#ff471a; color:#FFFFFF";>
+			<i class="search icon "></i>ค้นหา</button>
+		    <div class="or" ></div>
+	      	<button class="ui button big" style="background-color:#53c68c; color:#FFFFFF";>
+	      	<i class="crosshairs icon"></i>ค้นหาอู่ใกล้เคียง</button>
+	 	</div>
+    </div><br>
+
+	<!-- <div class="ui container"> 
+	    <div class="ui buttons">
+	    	<div class="ui huge action input" >
+		    	<input type="text huge" placeholder="Search for a car in town, postcode etc." style="width: 550px;">
+		    </div>
+		  		<button class="ui orange1 button big" >
+					<i class="search icon "></i>ค้นหา</button>
+		  		<div class="or" style="width: 550px;"></div>
+		  		<button class="ui green1 button big" > 
+			      	<i class="crosshairs icon"></i>ค้นหาอู่ใกล้เคียง</button>
+			
+		</div>
+	</div><br> -->
+
+    <div >
+    	<h1 class="ui horizontal header divider container">
+        	<a>ง่ายๆใน 3 ขั้นตอน</a>
+      	</h1>
+    </div><br>
+    <div class="ui container">
+    	<div class="ui three column stackable grid">
+        	<div class="column">
+        		<div class="ui card ">
+  					<div class="image">
+    					<img src="img/car.png">
+  					</div>
+  					<div class="content">
+    					<a class="header">อู่ใกล้บ้าน</a>
+  					</div>
+  				</div>
+			</div>
+			<div class="column">
+        		<div class="ui card">
+  					<div class="image">
+    					<img src="img/chat.jpg">
+  					</div>
+  					<div class="content">
+    					<a class="header">เลือกบริการ</a>
+  					</div>
+				</div>
+			</div>
+        	<div class="column">
+        		<div class="ui card">
+  					<div class="image">
+    					<img src="img/dollar.png">
+  					</div>
+  					<div class="content">
+    					<a class="header">ตกลงราคา</a>
+  					</div>
+				</div>
+			</div>
+    	</div>
+    </div>
+
+    <div class="ui inverted vertical footer segment" style="background-color:#e0e0d1;">
+    	<div class="ui container">
+      		<div class="ui stackable inverted divided equal height stackable grid">
+        		<div class="four wide column">
+          			<div class="image">
+    					<img class="ui tiny image" src="img/freelogo.png">
+    					<img class="ui tiny image" src="img/freelogo.png">
+    					<img class="ui tiny image" src="img/freelogo.png">
+    					<img class="ui tiny image" src="img/freelogo.png">
+  					</div>
+        		</div>
+        		<div class="four wide column">
+          			<div class="image">
+    					<img class="ui tiny image" src="img/freelogo.png">
+    					<img class="ui tiny image" src="img/freelogo.png">
+    					<img class="ui tiny image" src="img/freelogo.png">
+    					<img class="ui tiny image" src="img/freelogo.png">
+  					</div>
+        		</div>
+        	<div class="five wide column">
+          		<div class="image">
+    				<img class="ui Large right floated image" src="img/freevideo.png">
+  				</div>
+        	</div>
+      	</div>
+    </div>
+
+    <div class="ui inverted vertical footer segment" style="background-color:#53c68c;">
+    	<div class="ui container">
+      		<div class="ui stackable inverted divided equal height stackable grid">
+        		<div class="four wide column">
+          			<h4 class="ui inverted header">เกี่ยวกับเรา</h4>
+          			<div class="ui inverted link list">
+			            <a href="#" class="item">ไซต์แมป</a>
+			            <a href="#" class="item">ติดต่อเรา</a>
+			            
+          			</div>
+        		</div>
+        		<div class="four wide column">
+          			<h4 class="ui inverted header">บริการ</h4>
+          			<div class="ui inverted link list">
+			            <a href="#" class="item">อู่รถยนตร์</a>
+			            <a href="#" class="item">ช่างซ่อม</a>
+			            <a href="#" class="item">นัดหมาย</a>
+			            <a href="#" class="item">ครังอะไหล่</a>
+
+          			</div>
+        		</div>
+        	<div class="five wide column">
+          		<h4 class="ui inverted header">ที่อยู่</h4>
+          		<div class="ui inverted link list">
+	          		<a href="#" class="item ">Tel 083000000</a>
+	          		<a href="#" class="item"><i class="facebook icon blue"></i>Carjaidee</a>
+	          		<a href="#" class="item"><i class="twitter icon teal"></i>Twitter</a>
+	          		<a href="#" class="item"><i class="google plus icon red"></i>Mail</a>
+	          		<a href="#" class="item"><i class="youtube icon red"></i>Youtube</a>
+          		</div>
+        	</div>
+      	</div>
+    </div>
+
+  </div>
+
+</body>
+</html>
